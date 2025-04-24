1c38b9b49e5568457facf1b06fafaad532e2ddc0
Lucas Quintanilha <l.quitan@gmail.com>
2025-04-23 23:08:43 -0300

    ✨feat: add get user profile service

---
+++ src/services/get-user-profile-service.ts
@@ -0,0 +1,29 @@
+import { UsersRepository } from '@/repositories/users-repository'
+import { User } from '@prisma/client'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+
+interface GetUserProfileServiceRequest {
+  id: string
+}
+
+interface GetUserProfileServiceResponse {
+  user: User
+}
+
+export class GetUserProfileService {
+  constructor(private usersRepository: UsersRepository) {}
+
+  async execute({
+    id,
+  }: GetUserProfileServiceRequest): Promise<GetUserProfileServiceResponse> {
+    const user = await this.usersRepository.findById(id)
+
+    if (!user) {
+      throw new ResourceNotFoundError()
+    }
+
+    return {
+      user,
+    }
+  }
+}
